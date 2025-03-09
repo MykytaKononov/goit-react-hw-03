c9ae554d367fe55a3d7753e69f5c86f1cc535c52
MykytaKononov <nikitamaxkononov@gmail.com>
2025-03-09 20:27:43 +0100

    done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Contact from "../Contact/Contacts";
+import Contact from "../Contact/Contact";
 
 export default function ContactList({ contacts, onDelete }) {
   return (
